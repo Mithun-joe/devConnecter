7e0cf82a2c2e02a796370bffdafa09e632272a05
Mithun <joieeemithun98@gmail.com>
2020-12-30 23:49:50 +0530

    components added on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import {Fragment} from "react";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 
 const App = () => (
+    <Router>
     <Fragment>
-      <h1>App</h1>
+      <Navbar/>
+      <Route exact path='/' component={Landing}/>
+      <section className="container">
+          <Switch>
+              <Route exact path='/register' component={Register}/>
+              <Route exact path='/login' component={Login}/>
+          </Switch>
+      </section>
     </Fragment>
+    </Router>
   );
 
 
